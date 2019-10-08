bfd413dadf16f9443c15ee24dcf98650cba011f9
mpolatcan <mutlupolatcan@gmail.com>
2019-10-08 10:40:26 +0300

    alpine versions removed due to lack of glibc

--- build.sh
+++ build.sh
@@ -13,24 +13,27 @@ PIG_VERSIONS=(
     "0.17.0"
 )
 
-DISTS=(
-    "alpine"
-    "ubuntu"
+JAVA_VERSIONS=(
+    "8"
+    "9"
+    "10"
+    "11"
+    "12"
 )
 
-# $1: DIST
-# $2: PIG_VERSION
-# $3: HADOOP_VERSION
+# $1: PIG_VERSION
+# $2: HADOOP_VERSION
+# $3: JAVA_VERSION
 function build_image() {
-  sudo docker build -q -t mpolatcan/pig:$1-$2-hadoop-$3 --build-arg PIG_VERSION=$2 --build-arg HADOOP_VERSION=$3 ./$1/
-	sudo docker push mpolatcan/pig:$1-$2-hadoop-$3
-	sudo docker rmi mpolatcan/pig:$1-$2-hadoop-$3
+  sudo docker build -q -t mpolatcan/pig:$1-hadoop-$2-java$3 --build-arg PIG_VERSION=$1 --build-arg HADOOP_VERSION=$2 --build-arg JAVA_VERSION=$3 ./src/
+	sudo docker push mpolatcan/pig:$1-hadoop-$2-java$3
+	sudo docker rmi mpolatcan/pig:$1-hadoop-$2-java$3
 }
 
 for PIG_VERSION in ${PIG_VERSIONS[@]}; do
     for HADOOP_VERSION in ${HADOOP_VERSIONS[@]}; do
-        for DIST in ${DISTS[@]}; do
-            build_image $DIST $PIG_VERSION $HADOOP_VERSION
+        for JAVA_VERSION in ${JAVA_VERSIONS[@]}; do
+            build_image $PIG_VERSION $HADOOP_VERSION $JAVA_VERSION
         done
     done
 done
